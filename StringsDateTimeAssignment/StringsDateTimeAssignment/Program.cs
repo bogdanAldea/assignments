4a1ee74531d382323e181bb4c7d7ac41f75c7c86
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-18 13:23:00 +0300

    new assigment

---
+++ StringsDateTimeAssignment/StringsDateTimeAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿//String methods
+using StringsDateTimeAssignment;
+using System.Globalization;
+
+string uFirstName = "Bogdan";
+string uLastName = "Aldea";
+string uEmail = "bogdan.aldea@gmail.com";
+string uFavTeams = "Juventus, Liverpool, Bournemouth, Atalanta";
+decimal uSalary = 2100.0m;
+
+User user = new User(uFirstName, uLastName, uEmail, uFavTeams, uSalary);
+
+bool userEmailIsValid = user.EmailIsValid();
+Console.WriteLine($"Email entered for user {user.FullName()} is: {userEmailIsValid}");
+
+string username = user.GenerateUsername();
+Console.WriteLine($"Generated username for user {user.FullName()} is: {username}");
+
+
+// Split string and list as bullet points(before .Replace() method)
+user.ListFavTeamsAsBulletPoints();
+
+// .Replace() method and assign the old string to the new string
+user.ReplaceTeamInString("Atalanta", "Borrusia Dortmund");
+
+// Replace() method but with .Equal() method (assigning the same string to the old one)
+user.ReplaceTeamInString("Juventus", "juventus");
+
+// Split string and list as bullet points (after .Replace() 
+user.ListFavTeamsAsBulletPoints();
+
+// TimeSpan
+Console.WriteLine($"User not logged in for: {user.LastLogin}");
+
+// DateTime
+user.DateofBirth = new DateTime(day: 23, month: 6, year: 1993);
+Console.WriteLine(user.DateofBirth);
+
+// Currency CultureInfo
+Console.WriteLine($"User earns {user.FormatSalary()}");
+
+// Datetime Offset
+var d = new DateTimeOffset(dateTime: user.DateofBirth);
+Console.WriteLine(d);
+
---
+++ StringsDateTimeAssignment/StringsDateTimeAssignment/User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsDateTimeAssignment
+{
+    public class User
+    {
+        public User(string firstName, string lastName, string email, string favFootballTeams, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            FavFootballTeams = favFootballTeams;
+            Salary = salary;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string FavFootballTeams { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public TimeSpan LastLogin { get; set; } = new TimeSpan(days: 2, hours: 10, minutes: 5, seconds: 100);
+        public decimal Salary { get; set; }
+
+        public bool EmailIsValid()
+        {
+
+            // .Contains() method
+
+            if (Email.Contains("@gmail.com"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string GenerateUsername()
+        {
+
+            // .ToLower() method
+            // .Join() method
+
+            string[] userDetails = new string[] { FirstName.ToLower(), LastName.ToLower() };
+            return String.Join(".", userDetails);
+        }
+
+        public string FullName()
+        {
+            // String interpolation
+
+            return $"{FirstName} {LastName}";
+        }
+
+        public void ListFavTeamsAsBulletPoints()
+        {
+            // .Split() method
+
+            string[] teams = FavFootballTeams.Split(", ");
+            foreach (var team in teams)
+            {
+                Console.WriteLine($"[*] {team}");
+            }
+        }
+
+        public void ReplaceTeamInString(string oldTeam, string newTeam)
+        {
+            // .Replace() method
+            // .Equals() method
+
+            if (oldTeam.Equals(newTeam, StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine($"Trying to assign the same string: {oldTeam} => {newTeam}");
+            }
+
+            else
+            {
+                FavFootballTeams = FavFootballTeams.Replace(oldTeam, newTeam);
+            }
+        }
+
+        public string FormatSalary()
+        {
+            CultureInfo romCurency = new CultureInfo("ro-RO");
+            return Salary.ToString("C", romCurency);
+        }
+
+    }
+}
