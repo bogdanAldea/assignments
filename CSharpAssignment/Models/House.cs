1c5a7f7b5e8cd9cf255865078f631168b32a0fb5
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-26 09:19:13 +0300

    updated assignment

--- CSharpAssignment/Models/House.cs
+++ CSharpAssignment/Models/House.cs
@@ -7,12 +7,23 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class House : IHouse
+    public class House : IHouse, ICloneable
     {
         public bool HasYard { get; set; }
         public int Number { get; set; }
         public int Residents { get; set; }
         public double SurfaceArea { get; set; }
         public int Id { get; set; }
+
+        public object Clone()
+        {
+            IHouse house = new House();
+            house.Number = Number;
+            house.Residents = Residents;
+            house.SurfaceArea = SurfaceArea;
+            house.Id = Id;
+
+            return house;
+        }
     }
 }
